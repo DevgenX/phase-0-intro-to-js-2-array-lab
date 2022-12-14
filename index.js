dc4fb198ea3dbe78d4333f4f9922d3aac701e5c8
devgenx <sebastian110420@gmail.com>
2022-12-14 09:11:33 -0500

    commit tree 8215b8efba5c46c693b2dd1851b10f084e439458

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  return cats.shift();
+}
+
+function appendCat(name) {
+  let copyOfCats = cats.slice();
+  copyOfCats.push(name);
+  return copyOfCats;
+}
+
+function prependCat(name) {
+  let copyOfCats = cats.slice();
+  copyOfCats.unshift(name);
+  return copyOfCats;
+}
+
+function removeFirstCat() {
+  let newCats = cats.slice(1);
+  return newCats;
+}
+
+function removeLastCat() {
+  let newCats = cats.slice(0, cats.length - 1);
+  return newCats;
+}
